d9b852c28a93aa7bd83ec153ca899f9641259674
BZBro <brodenbenson@pm.me>
2026-09-02 17:41:45 -0600

    working on server integration and database integration

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const {DatabaseSync} = require('node:sqlite');
+const database = new DatabaseSync('user.db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
